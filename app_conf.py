0ab5e1bc8fb0629988af603f6dd91928d90e50cd
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2021-01-21 05:13:16 +0530

    Update app_conf.py

--- app_conf.py
+++ app_conf.py
@@ -1,8 +1,8 @@
 class AppSettings(object):
 
     # APP TOKENS
-    CONSUMER_KEY = 'mr0UPm5hlKqbgJrbmFDQbycbs'
-    CONSUMER_SECRET = ' dQFkWrwWDrdWRIuH9z9x9DqBOaKNphHDZzoXOXEXqLrqBQgHK5'
+    CONSUMER_KEY = ''
+    CONSUMER_SECRET = ' '
 
     # SESSION
     SECRET_KEY = '3faE64iwqJDNBGC5nQ60' # session encryption key
